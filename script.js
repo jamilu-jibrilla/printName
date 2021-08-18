da24968313f46b86eb76af954506bcb333e26def
jamil <jamilujibrilla01@gmail.com>
2021-08-18 11:49:07 +0000

    solution update

---
+++ script.js
@@ -0,0 +1,5 @@
+(function printName() {
+    let name = "Jamilu Jibrilla"
+    let nameContainer = document.querySelector(".nameContainer")
+    nameContainer.textContent = `Hi, my name is "${name}"` 
+})()
